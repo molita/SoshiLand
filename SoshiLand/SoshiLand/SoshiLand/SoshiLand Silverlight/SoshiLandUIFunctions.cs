b97cbd1e75ee8a2a33afdaafb3b11149ba669d39
Mark <bloodykiller2000@hotmail.com>
2011-09-19 04:13:47 -0400

    Got the zoom in working for the top row (minus corner pieces)

--- SoshiLand/SoshiLand/SoshiLand/SoshiLand Silverlight/SoshiLandUIFunctions.cs
+++ SoshiLand/SoshiLand/SoshiLand/SoshiLand Silverlight/SoshiLandUIFunctions.cs
@@ -27,40 +27,51 @@ namespace SoshiLandSilverlight
         private static int zoomWidth = (int)(470 * zoomRatio);
         private static int zoomHeight = (int)(800 * zoomRatio);
 
-        private static int leftSideOfBoard = ((Game1.preferredWindowWidth - (Game1.preferredWindowHeight / 2)) / 2);
-        private static int rightSideOfBoard = ((Game1.preferredWindowWidth - (Game1.preferredWindowHeight / 2)) / 2) + (Game1.backgroundWidth / 2);
-        private static int startOfSideRow = (int)(Game1.backgroundHeight * (ratioBlackBorder + ratioCornerBoxes));
-        private static int oneSideBox = (int)(Game1.backgroundHeight * (ratioBlackBorder + ratioSideBoxes));
-        private static int oneSideBoxIncludingBorder = (int)(Game1.backgroundHeight * ((ratioBlackBorder * 2) + ratioSideBoxes));
+        // Relative to the window size
+        private static int window_leftSideOfBoard = ((Game1.preferredWindowWidth - (Game1.preferredWindowHeight)) / 2);
+        private static int window_rightSideOfBoard = window_leftSideOfBoard + Game1.preferredWindowHeight;
+        private static int window_oneSideBox = (int)(Game1.preferredWindowHeight * (ratioBlackBorder + ratioSideBoxes));
+        private static int window_oneSideBoxIncludingBorder = (int)(window_oneSideBox + (ratioBlackBorder * 2));
+        private static int window_cornerBox = (int)(Game1.preferredWindowHeight * (ratioCornerBoxes));
+        private static int window_cornerBoxIncludingBorder = (int)(window_cornerBox + (ratioBlackBorder * 2));
+
+        // Relative to the original size
+        private static int orig_startOfSideRow = (int)(Game1.backgroundHeight * (ratioBlackBorder + ratioCornerBoxes));
+        private static int orig_oneSideBox = (int)(Game1.backgroundHeight * (ratioBlackBorder + ratioSideBoxes));
+        private static int orig_oneSideBoxIncludingBorder = (int)(Game1.backgroundHeight * ((ratioBlackBorder * 2) + ratioSideBoxes));
+        private static int orig_cornerBoxIncludingBorder = (int)(Game1.backgroundHeight * ((ratioBlackBorder * 2) + ratioCornerBoxes));
 
         public static Props MouseCursorHoverForZoom(MouseState mouseState)
         {
             // Check if the mouse is within bounds of the top row
             if (mouseState.Y <= (ratioSideHeight * Game1.preferredWindowHeight) && mouseState.Y >= 0)
             {
-                if (mouseState.X <= leftSideOfBoard + oneSideBoxIncludingBorder)
+                if (mouseState.X <= window_leftSideOfBoard)
                     return Props.None;
-                else if (mouseState.X <= leftSideOfBoard + (oneSideBoxIncludingBorder * 1))
+                else if (mouseState.X <= window_leftSideOfBoard + window_cornerBoxIncludingBorder)
+                    //return Props.FanMeeting;
+                    return Props.None;
+                else if (mouseState.X <= window_leftSideOfBoard + window_cornerBoxIncludingBorder + (window_oneSideBoxIncludingBorder * 1))
                     return Props.LaScala;
-                else if (mouseState.X <= leftSideOfBoard + (oneSideBoxIncludingBorder * 2))
+                else if (mouseState.X <= window_leftSideOfBoard + window_cornerBoxIncludingBorder + (window_oneSideBoxIncludingBorder * 2))
                     return Props.Bali;
-                else if (mouseState.X <= leftSideOfBoard + (oneSideBoxIncludingBorder * 3))
+                else if (mouseState.X <= window_leftSideOfBoard + window_cornerBoxIncludingBorder + (window_oneSideBoxIncludingBorder * 3))
                     return Props.Chance1;
-                else if (mouseState.X <= leftSideOfBoard + (oneSideBoxIncludingBorder * 4))
+                else if (mouseState.X <= window_leftSideOfBoard + window_cornerBoxIncludingBorder + (window_oneSideBoxIncludingBorder * 4))
                     return Props.TempleMount;
-                else if (mouseState.X <= leftSideOfBoard + (oneSideBoxIncludingBorder * 5))
+                else if (mouseState.X <= window_leftSideOfBoard + window_cornerBoxIncludingBorder + (window_oneSideBoxIncludingBorder * 5))
                     return Props.DamnoenMarket;
-                else if (mouseState.X <= leftSideOfBoard + (oneSideBoxIncludingBorder * 6))
+                else if (mouseState.X <= window_leftSideOfBoard + window_cornerBoxIncludingBorder + (window_oneSideBoxIncludingBorder * 6))
                     return Props.GreatWall;
-                else if (mouseState.X <= leftSideOfBoard + (oneSideBoxIncludingBorder * 7))
+                else if (mouseState.X <= window_leftSideOfBoard + window_cornerBoxIncludingBorder + (window_oneSideBoxIncludingBorder * 7))
                     return Props.TajMahal;
-                else if (mouseState.X <= leftSideOfBoard + (oneSideBoxIncludingBorder * 8))
+                else if (mouseState.X <= window_leftSideOfBoard + window_cornerBoxIncludingBorder + (window_oneSideBoxIncludingBorder * 8))
                     return Props.StatueLiberty;
-                else if (mouseState.X <= leftSideOfBoard + (oneSideBoxIncludingBorder * 9))
+                else if (mouseState.X <= window_leftSideOfBoard + window_cornerBoxIncludingBorder + (window_oneSideBoxIncludingBorder * 9))
                     return Props.Forever9;
-                else if (mouseState.X <= leftSideOfBoard + (oneSideBoxIncludingBorder * 10))
+                else if (mouseState.X <= window_leftSideOfBoard + window_cornerBoxIncludingBorder + (window_oneSideBoxIncludingBorder * 10))
                     return Props.EiffelTower;
-                else if (mouseState.X <= leftSideOfBoard + (oneSideBoxIncludingBorder * 11))
+                else if (mouseState.X <= window_leftSideOfBoard + window_cornerBoxIncludingBorder + (window_oneSideBoxIncludingBorder * 11))
                     return Props.Parthenon;
             }
 
@@ -87,8 +98,8 @@ namespace SoshiLandSilverlight
                 case Props.EiffelTower:
                 case Props.Parthenon:
                     spriteBatch.Draw(Game1.background,
-                        new Rectangle(rightSideOfBoard, 0*(zoomHeight / 2), zoomWidth, zoomHeight),
-                        new Rectangle(startOfSideRow + (positionFromLeftToRightOrTopToBottomExcludingCorner - 1) * oneSideBox, 0, (int)(Game1.backgroundWidth * (ratioSideBoxes + ratioBlackBorder * 2)), (int)(Game1.backgroundHeight * ratioSideHeight)),
+                        new Rectangle(window_rightSideOfBoard, (zoomHeight / 2), zoomWidth, zoomHeight),
+                        new Rectangle(orig_startOfSideRow + (positionFromLeftToRightOrTopToBottomExcludingCorner - 1) * orig_oneSideBox, 0, (int)(Game1.backgroundWidth * (ratioSideBoxes + ratioBlackBorder * 2)), (int)(Game1.backgroundHeight * ratioSideHeight)),
                         Color.White, MathHelper.ToRadians(180), new Vector2(zoomWidth / 2, zoomHeight / 2), SpriteEffects.None, 0f);
                     break;
 
@@ -97,6 +108,8 @@ namespace SoshiLandSilverlight
                     // Left Column Items
 
                     // Right Column Items
+                    //case Props.FanMeeting
+                    //spriteBatch.d
             }
         }
 
